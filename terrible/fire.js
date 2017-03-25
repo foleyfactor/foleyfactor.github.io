152d4a8b6034c10f9a5415dc009d3019110933f0
Alex Foley <foleyfactor98@gmail.com>
2017-03-25 18:48:50 -0400

    more terrible hacks stuff

--- terrible/fire.js
+++ terrible/fire.js
@@ -5,23 +5,26 @@ function httpGet(theUrl){
     return xmlHttp.responseText;
 }
 
+var fire = false;
+
 function checkfire(){
-  if(httpGet("https://foleyfactor.github.io/resume.pdf").result == "1"){
+	let maxV = JSON.parse(httpGet("https://api.particle.io/v1/devices/34001e001347343339383037/maxv?access_token=8b08209967c1f21b0acdbefabb634028eef09b85")).result;
+	let maxI = JSON.parse(httpGet("https://api.particle.io/v1/devices/34001e001347343339383037/maxi?access_token=8b08209967c1f21b0acdbefabb634028eef09b85")).result;
+  if (maxV > 1500 && maxI > 50){
     document.getElementById("fire").classList.add("visible");
     document.getElementById("fire").classList.remove("invisible");
     document.getElementById("thumb").classList.add("invisible");
     document.getElementById("thumb").classList.remove("visible");
+    fire = true;
   } else {
     document.getElementById("thumb").classList.add("visible");
     document.getElementById("thumb").classList.remove("invisible");
     document.getElementById("fire").classList.add("invisible");
     document.getElementById("fire").classList.remove("visible");
   }
-
-  let str = "";
-  str += JSON.parse(httpGet("https://api.particle.io/v1/devices/34001e001347343339383037/maxv?access_token=8b08209967c1f21b0acdbefabb634028eef09b85")).result + " " + JSON.parse(httpGet("https://api.particle.io/v1/devices/34001e001347343339383037/maxi?access_token=8b08209967c1f21b0acdbefabb634028eef09b85")).result;
-  if (str)
-    document.getElementById("data").innerHTML = str;
 }
 
-setInterval(checkfire,250);
+setInterval(() => {
+	if (!fire)
+		checkfire();
+	} ,250);
